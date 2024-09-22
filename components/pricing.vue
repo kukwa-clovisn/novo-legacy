645ed55e5365d733131f81a91673fb4e3b60a78a
kukwa clovis <kukwaclovisngong3@gmail.com>
2024-09-22 05:16:30 +0100

    new website update

--- components/pricing.vue
+++ components/pricing.vue
@@ -50,7 +50,7 @@
               >
                 <path d="M20 6L9 17l-5-5"></path>
               </svg> </span
-            >Vexillologist pitchfork
+            >3days a week
           </p>
           <p class="flex items-center text-gray-600 mb-2">
             <span
@@ -67,7 +67,7 @@
               >
                 <path d="M20 6L9 17l-5-5"></path>
               </svg> </span
-            >Tumeric plaid portland
+            >follow up
           </p>
           <p class="flex items-center text-gray-600 mb-6">
             <span
@@ -84,7 +84,7 @@
               >
                 <path d="M20 6L9 17l-5-5"></path>
               </svg> </span
-            >Mixtape chillwave tumeric
+            >vip inner circle
           </p>
           <button
             class="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded"
@@ -140,7 +140,7 @@
               >
                 <path d="M20 6L9 17l-5-5"></path>
               </svg> </span
-            >Vexillologist pitchfork
+            >3days a week
           </p>
           <p class="flex items-center text-gray-600 mb-2">
             <span
@@ -157,24 +157,7 @@
               >
                 <path d="M20 6L9 17l-5-5"></path>
               </svg> </span
-            >Tumeric plaid portland
-          </p>
-          <p class="flex items-center text-gray-600 mb-2">
-            <span
-              class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0"
-            >
-              <svg
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2.5"
-                class="w-3 h-3"
-                viewBox="0 0 24 24"
-              >
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg> </span
-            >Hexagon neutra unicorn
+            >follow up
           </p>
           <p class="flex items-center text-gray-600 mb-6">
             <span
@@ -191,7 +174,24 @@
               >
                 <path d="M20 6L9 17l-5-5"></path>
               </svg> </span
-            >Mixtape chillwave tumeric
+            >vip inner circle
+          </p>
+          <p class="flex items-center text-gray-600 mb-6">
+            <span
+              class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0"
+            >
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2.5"
+                class="w-3 h-3"
+                viewBox="0 0 24 24"
+              >
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg> </span
+            >yearly bonuses/giveaways
           </p>
           <button
             class="flex items-center mt-auto text-white bg-orange-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-orange-600 rounded"
@@ -209,9 +209,7 @@
               <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
           </button>
-          <p class="text-xs text-gray-500 mt-3">
-            Literally you probably haven't heard of them jean shorts.
-          </p>
+          <p class="text-xs text-gray-500 mt-3"></p>
         </div>
       </div>
     </div>
